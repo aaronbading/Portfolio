51de8e43d50137973f2966ffde8b3fbf7b06eaf3
aaronbading <aaronbading.ab@gmail.com>
2022-07-01 16:36:49 -0500

    Update information

--- dist/index.379dd93c.js
+++ dist/index.379dd93c.js
@@ -955,6 +955,6 @@ const targetElements = [
     }, 
 ];
 
-},{"@parcel/transformer-js/src/esmodule-helpers.js":"ciiiV"}]},["lBB98","hD4hw"], "hD4hw", "parcelRequire6aa4")
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"ciiiV"}]},["lBB98","hD4hw"], "hD4hw", "parcelRequire7c06")
 
 //# sourceMappingURL=index.379dd93c.js.map
